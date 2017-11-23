a4618f6732a18d1a7f9f52a000f223bbfd77a7c6
HENRIQUE SILVA <henrique.silva533@etec.sp.gov.br>
2017-11-22 22:31:56 -0200

    Algumas modificações no codigo fonte.

---
+++ EditorDeTextoCore/Arquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace EditorDeTextoCore
+{
+    public class Arquivo
+    {
+        public string Nome { get; set; } = "[sem nome]";
+        public string Diretorio { get; set; }
+        public bool Salvo { get; set; }
+        public string DiretorioCompleto { get {return Diretorio + Nome; } }
+        public string  Texto { get; set; }
+
+
+        public void Salvar()
+        {
+            var teste = File.Create(DiretorioCompleto);
+            teste.Close();
+            var escrever = new StreamWriter(DiretorioCompleto);
+            escrever.Write(Texto);
+            escrever.Close();
+        }
+
+        public static bool Existe (Arquivo arquivo)
+        {
+            return File.Exists(arquivo.DiretorioCompleto);
+        }
+
+       
+    }
+}
--- EditorDeTextoXaml/MainWindow.xaml.cs
+++ EditorDeTextoXaml/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EditorDeTextoCore;
+using Microsoft.Win32;
 
 namespace EditorDeTextoXaml
 {
@@ -20,9 +22,43 @@ namespace EditorDeTextoXaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string _CostTitulo = "Notepad + ou -";
+
+        delegate void delegado();
+
+        event delegado ArquivoModificado;
+
+
+        Arquivo arq = new Arquivo();
         public MainWindow()
         {
             InitializeComponent();
+            Title = _CostTitulo;
+            txtTexto.TextChanged += (s, e) => TextoModificado();
+        }
+
+        private void TextoModificado()
+        {
+            if (!txtTexto.Text.Equals(arq.Texto))
+                Title = _CostTitulo + "*" + arq.Nome;
+            else
+                Title = _CostTitulo + arq.Nome;
+        }
+
+        private void Salvar(object sender, RoutedEventArgs e)
+        {
+            if (!Arquivo.Existe(arq))
+            {
+                var arquivo = new SaveFileDialog();
+                arquivo.Filter = "Arquivos de texto|*.txt";
+                arquivo.ShowDialog();
+                arq.Nome = arquivo.SafeFileName;
+                arq.Diretorio = arquivo.InitialDirectory;
+                
+            }
+            arq.Texto = txtTexto.Text;
+            arq.Salvar();
+            TextoModificado();
         }
     }
 }
